784cca69367b85cf5f170b63247630828d9a4600
Wondo00o <ehdnjsok@naver.com>
2025-03-15 23:28:52 +0900

    Add files via upload

--- mynamewondo.py
+++ mynamewondo.py
@@ -1,59 +1,12 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "288f510a-db3c-41ca-8d18-cf13e4bcca0f",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "나의 이름은: 김도원\n",
-      "나의 나이는: 22\n",
-      "나의 키는: 155\n",
-      "10+20= 30\n",
-      "10*20= 200\n"
-     ]
-    }
-   ],
-   "source": [
-    "print('나의 이름은:','김도원')\n",
-    "print('나의 나이는:','22')\n",
-    "print('나의 키는:','155')\n",
-    "print('10+20=',10+20)\n",
-    "print('10*20=',10*20)\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "be033e97-c70f-4d21-892a-0fca2a0416bf",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('나의 이름은:','김도원')
+나의 이름은: 김도원
+>>> print('나의 나이는:','22')
+나의 나이는: 22
+>>> print('나의 키는:','155','cm입니다.')
+나의 키는: 155 cm입니다.
+>>> print('10+20=',10+20)
+10+20= 30
+>>> print('10*20=',10*20)
+10*20= 200
