e49cd7bc446f057bd2079ee17af3abfa023ce5ed
Wondo00o <ehdnjsok@naver.com>
2025-03-15 23:51:56 +0900

    Add files via upload

---
+++ mathwondo00o.py
@@ -0,0 +1,24 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/oem/Downloads/wondo/mathwondo.py ==============
+>>> print(123*456)
+56088
+>>> print(1357+2458)
+3815
+>>> print(5**4)
+625
+>>> print(10/4)
+2.5
+>>> print(10//5)
+2
+>>> print(10%5)
+0
+>>> print(5%2)
+1
+>>> print(2**1/2)
+1.0
+>>> print(2**0.5)
+1.4142135623730951
+>>> print(3**0.5)
+1.7320508075688772
