f8d17f722c2d9438ee00eea6ba9c978d5b168d9c
Wondo00o <ehdnjsok@naver.com>
2025-03-15 23:31:47 +0900

    Add files via upload

---
+++ wondocircle.py
@@ -0,0 +1,9 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> radius=6.0
+>>> print('원의 반지름',radius)
+원의 반지름 6.0
+>>> print('원의 면적',3.14*radius*radius)
+원의 면적 113.03999999999999
+>>> print('원의 둘레',2.0*3.14*radius)
+원의 둘레 37.68
