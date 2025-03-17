f9463a3ba2bfe2566bfc545cce27326df5050b49
Wondo00o <ehdnjsok@naver.com>
2025-03-17 16:21:51 +0900

    Add files via upload

---
+++ wondo250317-5.py
@@ -0,0 +1,18 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("a   n   a**n")
+... 
+a   n   a**n
+>>> print("-" * 15)
+... 
+---------------
+>>> n = 2
+... 
+>>> for a in range(2, 7):
+...     print(f"{a:<3} {n:<3} {a**n:<5}")
+... 
+2   2   4    
+3   2   9    
+4   2   16   
+5   2   25   
+6   2   36   
