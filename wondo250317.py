2b02a9ed6dee3959b6bd8cea7a039899d8d9d5dc
Wondo00o <ehdnjsok@naver.com>
2025-03-17 10:28:36 +0900

    Add files via upload

---
+++ wondo250317.py
@@ -0,0 +1,10 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(200,'+',300,'+',400,'=',200+300+400)
+200 + 300 + 400 = 900
+>>> weight=30
+>>> height=60
+>>> print(weight)
+30
+>>> print(height)
+60
