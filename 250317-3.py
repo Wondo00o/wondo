0d097cfec0096abba70d927a45488c30b5683866
Wondo00o <ehdnjsok@naver.com>
2025-03-17 10:51:00 +0900

    Add files via upload

---
+++ 250317-3.py
@@ -0,0 +1,7 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> width=40
+>>> height=20
+>>> 삼각형면적=width*height*1/2
+>>> print(삼각형면적)
+400.0
