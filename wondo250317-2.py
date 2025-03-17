d24032758b5f77b39e941bbddb0110fc32a63f5f
Wondo00o <ehdnjsok@naver.com>
2025-03-17 10:44:48 +0900

    Add files via upload

---
+++ wondo250317-2.py
@@ -0,0 +1,7 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> width=30
+>>> height=60
+>>> 사각형의면적=width*height
+>>> print('사각형의 면적:',width*height)
+사각형의 면적: 1800
